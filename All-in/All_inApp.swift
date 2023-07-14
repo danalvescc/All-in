2ff0a91156d6b0311189af94a2ce39c8d91b42a8
Daniel Alves <danalvescc@gmail.com>
2023-07-14 16:19:19 -0300

    Initial Commit

---
+++ All-in/All_inApp.swift
@@ -0,0 +1,17 @@
+//
+//  All_inApp.swift
+//  All-in
+//
+//  Created by Daniel  Alves Barreto on 14/07/23.
+//
+
+import SwiftUI
+
+@main
+struct All_inApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ All-in/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  All-in
+//
+//  Created by Daniel  Alves Barreto on 14/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
